cc09407b5a69ba7eec60cac5d51c8b6755bde85a
Ranindu Nethmina <ranindunethmina@gmail.com>
2024-11-13 18:46:46 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/crop_monitoring_system/CropMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_monitoring_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitoringSystemApplication.class, args);
+	}
+
+}
